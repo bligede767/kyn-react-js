84c4da2b47a3e4aa585ce402df800e87bcf684c9
radwija <115165737+radwija@users.noreply.github.com>
2023-05-26 03:03:39 +0800

    fixing search feature

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -15,7 +15,7 @@ const SearchBar = () => {
     const onSubmit = async (e) => {
         e.preventDefault();
         const filter = e.target.filter.value;
-        
+
         let carNameKeyword = e.target.carNameKeyword.value;
         let modelKeyword = '';
         let makeYearKeyword = '';
@@ -25,14 +25,19 @@ const SearchBar = () => {
 
         if (isInSearch) {
             carNameKeyword = e.target.carNameKeyword.value;
-          } else {
+            modelKeyword = e.target.modelKeyword.value;
+            makeYearKeyword = e.target.makeYearKeyword.value;
+            price = e.target.price.value;
+            min = e.target.min.value;
+            max = e.target.max.value;
+        } else {
             carNameKeyword = e.target.carNameKeyword?.value || '';
             modelKeyword = e.target.modelKeyword?.value || '';
             makeYearKeyword = e.target.makeYearKeyword?.value || '';
             price = e.target.carNameKeyword?.value || '';
             min = e.target.min?.value || '';
             max = e.target.max?.value || '';
-          }
+        }
 
         if (filter == "carname") {
             console.log(`car name: ${filter} ${carNameKeyword}`)
@@ -61,7 +66,7 @@ const SearchBar = () => {
                 <div>
                     <div>
                         <input type="hidden" id="carname" name="filter" value="carname" />
-                        <input type="text" name="carNameKeyword" required={selectedFilter === 'carname'} />
+                        <input type="text" name="carNameKeyword" placeholder='Search' required />
                     </div>
                 </div>) : (
 
@@ -69,23 +74,23 @@ const SearchBar = () => {
                     <div>
                         <input type="radio" id="carname" name="filter" value="carname" onChange={handleRadioChange} />
                         <label htmlFor="carname">Car Name</label>
-                        <input type="text" name="carNameKeyword" required={selectedFilter === 'carname'} />
+                        <input type="text" name="carNameKeyword" placeholder='Car Name' required={selectedFilter === 'carname'} />
                     </div>
                     <div>
                         <input type="radio" id="model" name="filter" value="model" onChange={handleRadioChange} />
                         <label htmlFor="model">Model</label>
-                        <input type="text" id="anjing" name="modelKeyword" required={selectedFilter === 'model'} />
+                        <input type="text" id="anjing" name="modelKeyword" placeholder='Model' required={selectedFilter === 'model'} />
                     </div>
                     <div>
                         <input type="radio" id="makeyear" name="filter" value="makeyear" onChange={handleRadioChange} />
                         <label htmlFor="makeyear">Make Year</label>
-                        <input type="text" name="makeYearKeyword" required={selectedFilter === 'makeyear'} />
+                        <input type="text" name="makeYearKeyword" placeholder='Make Year' required={selectedFilter === 'makeyear'} />
                     </div>
                     <div>
                         <input type="radio" id="price" name="filter" value="price" onChange={handleRadioChange} />
                         <label htmlFor="price">Price</label>
-                        <input type="number" name="min" />
-                        <input type="number" name="max" />
+                        <input type="number" placeholder='Minimum Price' name="min" />
+                        <input type="number" placeholder='Maximum Price' name="max" />
                     </div>
                 </div>
             )}
